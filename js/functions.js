1b63ac77fb7ad2e983d1d8e5fca22ef85e3e28ff
marcelsafont <marcel.safont1983@gmail.com>
2019-09-26 20:24:21 +0200

    work in progress responsive

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,7 @@ $(window).load(function() {
 	$('.flexslider').flexslider({
 		animation: "fade",
 	    start: function (slider) {
-	    	$('.content-slide input').change(function(event){
+	    	$('.content-slide .box-checkbox').click(function(event){
 	        	$('.flexslider').flexslider('next')
 	    	})
 	  	}
@@ -10,5 +10,8 @@ $(window).load(function() {
 });
 
 $(document).ready(function(){
+	$('.box-checkbox').click(function(){
+		$(this).toggleClass('checked');
+	})
 	$("#container-video").fitVids();
 });
